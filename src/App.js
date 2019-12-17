2057749c8bf48508c6a824acd64677ef6031e450
David Asare <gohan.david11@gmail.com>
2019-12-17 15:38:29 +0000

    Added team section plus adding social media icons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import MyNavbar from "./components/navbar";
 import TopContent from "./components/top-content";
 import About from "./components/about";
 import Service from "./components/service";
+import Team from "./components/team";
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
       </div>
       <div className="service">
         <Service />
+      </div>
+      <div className="team">
+        <Team />
       </div>
     </div>
   );
