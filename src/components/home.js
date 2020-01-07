8834e803c1afa26a367ac87b2fc717e4db6cde28
David Asare <gohan.david11@gmail.com>
2020-01-07 16:03:25 +0000

    Added homepage so I can keep my homepage components in

---
+++ src/components/home.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { TopContent } from "./top-content";
+import AboutSection from "./aboutSection";
+import ServiceSection from "./serviceSection";
+import TeamSection from "./teamSection";
+import MembershipSection from "./membershipSection";
+import ContactSection from "./contactSection";
+import "../home.css";
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="home">
+          <header className="top-content ">
+            <TopContent />
+          </header>
+          <AboutSection />
+          <ServiceSection />
+
+          <TeamSection />
+
+          <MembershipSection />
+
+          <ContactSection />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
