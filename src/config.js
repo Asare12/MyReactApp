d2691140a1d3bdd8a277c5910f5897d54f4a84fc
David Asare <gohan.david11@gmail.com>
2020-01-12 21:18:41 +0000

    applying aws config

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export default {
+  //   apiGateway: {
+  //     REGION: "eu-west-2",
+  //     URL: "YOUR_API_GATEWAY_URL"
+  //   },
+  cognito: {
+    REGION: "eu-west-2",
+    USER_POOL_ID: "eu-west-2_L5bIrtr4K",
+    APP_CLIENT_ID: "36u6kf7rrp6flkqpo8evm54fru",
+    IDENTITY_POOL_ID: "eu-west-2:51436f24-c782-4c61-a64d-79ea942bcd82"
+  }
+};
